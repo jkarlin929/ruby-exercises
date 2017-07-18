27c26462e30943098af660e15df91276238ac0c1
jkarlin929 <osida11572@gmail.com>
2017-07-18 19:51:32 -0400

    Release 1 complete

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,3 +1,21 @@
-hamster_name = nil
 puts "What's your hamsters name?"
 hamster_name = gets.chomp
+
+puts "On a scale of 1 to 10, how loud is #{hamster_name}"
+volume_level = gets.to_i
+
+puts "What is the fur color?"
+fur_color = gets.chomp
+
+puts "Is there a good candidate for adoption?"
+adoption = gets.chomp
+
+if adoption == "yes"
+    adoption = true
+else
+  adoption = false
+end
+
+age = nil
+puts "How old is the hamster?"
+age = gets.to_i
