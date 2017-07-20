bb12479dff608485d4b949fe84113f2b7c6167b4
jkarlin929 <osida11572@gmail.com>
2017-07-19 20:35:59 -0400

    Added Some Code

---
+++ ruby/vampires.rb
@@ -0,0 +1,18 @@
+puts "What is your name?"
+first_name = gets.chomp
+
+puts "What year were you born?"
+year_born = gets.to_i
+
+puts "Do you like garlic bread, true or false?"
+gets.chomp
+bread_choice = #{gets.chomp}
+
+puts "Would you like to enroll in health insurance, true or false?"
+gets.chomp
+insurance = #{gets.chomp}
+
+if
+  year_born > 1900 || (bread_choice == true || insurance == true)
+    puts "Probably not a vampire"
+end
