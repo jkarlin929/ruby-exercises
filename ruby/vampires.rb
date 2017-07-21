673bf40db3494823bd12a5e26ac4fee48eae42ae
jkarlin929 <osida11572@gmail.com>
2017-07-21 18:00:39 -0400

    added more conditions finally

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,28 +1,22 @@
 puts "What is your name?"
-first_name = gets.chomp
+name = gets.chomp
 
 puts "What year were you born?"
 year_born = gets.to_i
 
 puts "Do you like garlic bread, true or false?"
-gets.chomp
-bread_choice = #{gets.chomp}
+bread_choice = gets.chomp
 
-puts "Would you like to enroll in health insurance, true or false?"
-gets.chomp
-insurance = #{gets.chomp}
+puts "Would you like to enroll in health insurance, true or false"
+insurance = gets.chomp
 
 if
-  year_born > 1900 || (bread_choice == true && insurance == true)
-      puts "Probably not a vampire."
-elsif
-  year_born < 1900 || (bread_choice == false || insurance == false)
-      puts "Probably a vampire."
-elsif
-  year_born < 1900 && (bread_choice == false && insurance == false)
-      puts "Almost certainly a vampire."
-
-
-
+  year_born > 1900
+    puts "Probably not a vampire"
+  elsif bread_choice == "true" || insurance == "true"
+    puts "Probably a vampire"
+  elsif bread_choice == "false" && insurance == "false"
+    puts "Almost certainly a vampire"
+  else
+    puts "Results inconclusive"
 end
-
