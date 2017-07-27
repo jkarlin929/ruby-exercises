59f3781fa9e6774e234fd61f557fca818d69332e
jkarlin929 <osida11572@gmail.com>
2017-07-26 20:31:16 -0400

    Initial Commit

---
+++ ruby/hashes.rb
@@ -0,0 +1,24 @@
+#Details for Interior Designer
+
+#Put Client's name, age, number of children, decor theme, email address, year apartment was built and if they want ice cream drip-proof technology installed
+
+puts "What is your name?"
+name = gets.chomp
+
+puts "What is your age?"
+age = gets.to_i
+
+puts "How many children do you have?"
+children = gets.to_i
+
+puts "Which decor theme do you prefer?"
+decor_theme = gets.chomp
+
+puts "What is your email address?"
+email = gets.chomp
+
+puts "What year was your apartment built?"
+year_built = gets.to_i
+
+puts "Do you want ice cream drip-proof technology (put true or false)"
+technology = gets.chomp
