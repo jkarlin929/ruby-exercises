b80fe18d727cea17e4e16c7f9bb273866fe90441
jkarlin929 <osida11572@gmail.com>
2017-07-29 20:26:12 -0400

    Initial Commit

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,8 @@
+#enter name
+name = "Homer Simpson"
+
+#swap first and last name
+
+#change the vowels to the following vowel in the alphabet. remember to handle edge cases
+
+#change the consanants to the following consanants, excluding any vowels
