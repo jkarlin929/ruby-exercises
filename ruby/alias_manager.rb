5ef2a0023d4f0ed018be022ae6db44e7c24ef4d3
jkarlin929 <osida11572@gmail.com>
2017-07-29 22:38:31 -0400

    swapped first and last name

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,8 +1,60 @@
+#Fake Name Generator
+
 #enter name
 name = "Homer Simpson"
 
+
 #swap first and last name
+fake_name = name.downcase.split.reverse
+p fake_name
 
-#change the vowels to the following vowel in the alphabet. remember to handle edge cases
+#change the vowels to the following vowel in the alphabet. Remember to handle edge cases.
 
-#change the consanants to the following consanants, excluding any vowels
+letters = {
+  vowels: [
+    "a",
+    "e",
+    "i",
+    "o",
+    "u"
+  ],
+  consanants: [
+    "b",
+    "c",
+    "d",
+    "f",
+    "g",
+    "h",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z"
+  ]
+}
+
+vowels = ["a", "e", "i", "o", "u"]
+new_vowels = [  ]
+
+puts "Original data:"
+p letters [:vowels]
+p new_vowels
+
+letters[:vowels].map do |vowel|
+  new_vowels << vowel.next
+end
+
+puts "New data:"
+p vowels
+p new_vowels
+#change the consanants to the following consanants, excluding any vowels. Remember to handle edge cases.
