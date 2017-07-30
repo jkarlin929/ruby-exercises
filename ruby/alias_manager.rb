8d34af5c31ee5e951195a8b047dd56ca31c515a7
jkarlin929 <osida11572@gmail.com>
2017-07-30 14:59:50 -0400

    Added some methods to test

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,60 +1,68 @@
-#Fake Name Generator
+#Fake Spy Name Generator
 
 #enter name
 name = "Homer Simpson"
-
-
-#swap first and last name
-fake_name = name.downcase.split.reverse
+p name
+letters = {
+   vowels: [
+      "a",
+      "e",
+      "i",
+      "o",
+      "u"
+ ],
+   consanants: [
+      "b",
+      "c",
+      "d",
+      "f",
+      "g",
+      "h",
+      "j",
+      "k",
+      "l",
+      "m",
+      "n",
+      "p",
+      "q",
+      "r",
+      "s",
+      "t",
+      "v",
+      "w",
+      "x",
+      "y",
+      "z"
+ ]
+}
+#swap first and last name and lowercase the letters.
+#separate each letter by character.
+fake_name = "Homer Simpson".downcase.split.reverse
 p fake_name
+
+last_name = fake_name[0].chars
+first_name = fake_name[1].chars
+
+p last_name
+p first_name
+
+#p last_name = ["s", "i", "m", "p", "s", "o", "n"]
+#p first_name = ["h", "o", "m", "e", "r"]
+p letters[:vowels]
+p letters[:consanants]
+p letters[:vowels].rotate
+p letters[:consanants].rotate
+
+
+
 
 #change the vowels to the following vowel in the alphabet. Remember to handle edge cases.
 
-letters = {
-  vowels: [
-    "a",
-    "e",
-    "i",
-    "o",
-    "u"
-  ],
-  consanants: [
-    "b",
-    "c",
-    "d",
-    "f",
-    "g",
-    "h",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z"
-  ]
-}
 
-vowels = ["a", "e", "i", "o", "u"]
-new_vowels = [  ]
+#puts "Original data:"
+#p letters[:vowels]
 
-puts "Original data:"
-p letters [:vowels]
-p new_vowels
 
-letters[:vowels].map do |vowel|
-  new_vowels << vowel.next
-end
-
-puts "New data:"
-p vowels
-p new_vowels
+#puts "New data:"
+#p letters[:vowels]
 #change the consanants to the following consanants, excluding any vowels. Remember to handle edge cases.
