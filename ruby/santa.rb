bc30a9d23d49fbbd4c62a38a76c872b4b892dbb5
jkarlin929 <osida11572@gmail.com>
2017-08-05 17:31:21 -0400

    Added loop and unsuccessfully added random age

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,10 +1,13 @@
 class Santa
+  attr_reader :get_mad_at, :ethnicity
+  attr_accessor :gender
+
   def initialize(gender, ethnicity)
    @gender = gender
-   p gender
   puts "Initializing Santa instance ..."
-  @ethnicity = ethnicity
-  p ethnicity
+   @ethnicity = ethnicity
+ p gender
+ p ethnicity
  end
 
  def speak
@@ -20,12 +23,17 @@ class Santa
   reinder_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
   end
 
- def age
+ def age(n)
   age = 0
  end
 
+#getter method with added
+ # def celebrate_birthday
+ #  @age = age + 1
+ # end
 
 end
+
 #Initialize Santa class
 Santa1 = Santa.new("male", "polynesian")
 
@@ -34,4 +42,29 @@ p initialize
 puts Santa1.speak
 p Santa1.eat_milk_and_cookies("Snickerdoodle")
 p Santa1.reinder_ranking
-p Santa1.age
+p Santa1.age("")
+#p Santa1.celebrate_birthday
+p Santa1.get_mad_at
+#p Santa1.age
+p Santa1.ethnicity
+p Santa1.gender = "Trans"
+
+#Add more Santas to the class with genders and ethnicity values
+santas = []
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A", "female", "Gender queer"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A", "Inuit", "South African"]
+example_genders.length.times do |i|
+  santas << Santa.new(example_genders[i], example_ethnicities[i])
+end
+
+santas = 0
+
+until santas > 10
+puts "New Santa data:"
+p example_genders.sample
+p example_ethnicities.sample
+p age("").rand(0...140)
+santas += 1
+
+end
+
