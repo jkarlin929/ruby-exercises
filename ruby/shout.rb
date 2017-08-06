b61af1356e81bd962c3a99ca0b3f516ca933cd77
jkarlin929 <osida11572@gmail.com>
2017-08-05 21:05:00 -0400

    Finished Release 1

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,12 @@
 module Shout
-  #we'll put something here soon
+  def self.yell_angrily(words)
+    words + "!!!" + ":("
+  end
+
+  def self.yell_happily(words)
+    words + "!!!!!" + ":) :) :)"
+  end
 end
+
+p Shout.yell_angrily("OMFG")
+p Shout.yell_happily("Finally")
