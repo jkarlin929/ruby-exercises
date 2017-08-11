7f9f56465fdce5248d349583c337347537cbe249
jkarlin929 <osida11572@gmail.com>
2017-08-11 18:42:35 -0400

    working on release 1

--- js/algos.js
+++ js/algos.js
@@ -24,3 +24,17 @@ console.log(longestWord);
 // I ended up only being able to loop through an array I declared and looping through it.
 
 //Release 1
+
+//write a function that takes two objects(hashes in ruby) and checks to see if the objects share at least one key-value pair.
+
+//create an object inside of a function. use the name and age as keys to match.
+
+function yourFunction(object1, object2) {
+this.object1 = object1;
+this.object2 = object2;
+
+}
+var object1 = {name: 'Steven', age: 54};
+var object2 = {animal: 'dog',  age: 5};
+
+console.log(yourFunction(object1, object2));
