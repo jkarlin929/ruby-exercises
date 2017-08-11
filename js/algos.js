273102b2d68a0d4a45fea0420b1eb59b8deb40d2
jkarlin929 <osida11572@gmail.com>
2017-08-11 16:18:33 -0400

    Release 0 kind of Complete

--- js/algos.js
+++ js/algos.js
@@ -1,12 +1,15 @@
-//write a function that takes an array of words and returns the longest word in the array.
+//Release 0
+
+// write a function that takes an array of words and returns the longest word in the array.
+
 
 var fishArray = ["Cod", "Tuna", "Salmon", "Swordfish"];
 
 console.log(fishArray);
 
 var longest = 0;
-
-//loop through the array to find the longest word
+var array = [];
+//loop through the array to find the longest word by how many letters each string contains
 
 for (var i = 0; i < fishArray.length; i++) {
   if (fishArray[i].length > longest) {
@@ -17,3 +20,7 @@ for (var i = 0; i < fishArray.length; i++) {
 
 // console.log(longest);
 console.log(longestWord);
+
+// I ended up only being able to loop through an array I declared and looping through it.
+
+//Release 1
