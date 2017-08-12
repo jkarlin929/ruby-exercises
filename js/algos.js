69bf332587bfaf0d0183886fecaa7b2f3622f062
jkarlin929 <osida11572@gmail.com>
2017-08-11 22:54:35 -0400

    Release 1 not complete but close I think

--- js/algos.js
+++ js/algos.js
@@ -29,12 +29,26 @@ console.log(longestWord);
 
 //create an object inside of a function. use the name and age as keys to match.
 
-function yourFunction(object1, object2) {
+function compareObject(object1, object2) {
+this.compareObject;
 this.object1 = object1;
 this.object2 = object2;
-
-}
 var object1 = {name: 'Steven', age: 54};
 var object2 = {animal: 'dog',  age: 5};
+if (object1.name == object2.name){
+return false;
+}
+}
+var object1 = {name: 'Steven', age: 54};
+var object2 = {name: 'Fido',  age: 5};
 
-console.log(yourFunction(object1, object2));
+console.log(compareObject(object1, object2));
+
+//I definetely felt like if I had more time I could get this right. Right now it doesn't. I just really need to study more for practice assessment.
+
+
+//Release 2
+
+//Write a function that takes an integer for length, and builds and returns an array of strings of the given length.
+
+function length(x)
