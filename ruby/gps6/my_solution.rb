26b6aecd9a848aec2b7bbaa44e72e007f06c13cd
jkarlin929 <osida11572@gmail.com>
2017-08-12 22:00:15 -0400

    Added Release 2 through 8

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -4,27 +4,25 @@
 # We spent [#] hours on this challenge.
 
 # EXPLANATION OF require_relative
-#
-#
+#All answers for GPS under REFLECTION on bottom. Except #Release 3.
+
 require_relative 'state_data'
-#Release 1
-#require relative links the state data file with this #file. It allows the state_date file to loaded into the #class design. Require relative and require differ in #that require uses the current directory that you are #running the program from and require_relative uses the #directory of where that program itself resides
 
 class VirusPredictor
-
+#The method below initializes our instance variables.
   def initialize(state_of_origin, population_density, population)
     @state = state_of_origin
     @population = population
     @population_density = population_density
   end
-
+#The method below prints the other two methods with their #instance variables as their parameters.
   def virus_effects
     predicted_deaths(@population_density, @population, @state)
     speed_of_spread(@population_density, @state)
   end
 
   private
-
+#the method below predicts the deaths by determining the #population density against the population times a float, #which is then rounded to the nearest integer.
   def predicted_deaths(population_density, population, state)
     # predicted deaths is solely based on population density
     if @population_density >= 200
@@ -42,7 +40,7 @@ class VirusPredictor
     print "#{@state} will lose #{number_of_deaths} people in this outbreak"
 
   end
-
+#the method below determins the speed of which the virus spreads by determining the population density against an integer or float. If the
   def speed_of_spread(population_density, state) #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
@@ -83,7 +81,41 @@ california.virus_effects
 
 alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
 alaska.virus_effects
-
+#I know that release 4 is not supposed to look like this. I #can't quite grasp how to do this without someone else, OR #if I had some more time. I would think I would have to #include the instance variables and make them accessible #outside the class.
+all_states = STATE_DATA.each do |key, value|
+end
+puts all_states
 
 #=======================================================================
 # Reflection Section
+
+#Release 1
+#require relative links the state data file with this
+#file. It allows the state_date file to loaded into the #class design. Require relative and require differ in
+#that require uses the current directory that you are #running the program from and require_relative uses the #directory of where that program itself resides.
+
+#Release 2
+#This hash is taking each state, as a key, and then has #values of population density and population. Population #density and population are also their own keys which each #have an integer as values. It is a nested data structure of #a hash within another hash. STATE_DATA is the parent hash I think. It has a local scope.
+
+#Release 3
+#See below each instance method for responses.
+
+#Release 4
+#See end of DRIVER CODE
+
+#Release 5
+#I don't know.
+
+#Release 6
+#Private, when moved causes an error and cannot read the
+#driver code. When Private is used the method cannot be called outside the class scope. Private means the method is #important to the object's internal implementation, but they are not the concern of the outside world.
+
+#Release 7
+#I don't know.
+
+#Release 8
+#The differences between the two different hash syntaxes #shown in the state_data file are that they use both the #'=>' and the ':' symbols.
+#Require_relative links a file to another file. Require is #a complement of require_relative. Require doesn't offer a #good solution for loading files within a project's code.
+#Ways to iterate through a hash are using the '.each' or #'.map'method and entering the key/value pairs or just one #of the two.
+#I couldn't figure out how to refactor the virus_effects #method.
+#I can tell you I didn't solidify my refactoring #techniques.
--- ruby/gps6/state_data.rb
+++ ruby/gps6/state_data.rb
@@ -55,6 +55,3 @@ STATE_DATA = {
   "Wisconsin" => {population_density: 105.2, population: 5726398},
   "Wyoming" => {population_density: 5.851, population: 576412}
 }
-
-#Release 2
-#This hash is taking each state
