3c954b9988240ff0c564740b1ba2fb44c8644ebf
jkarlin929 <osida11572@gmail.com>
2017-08-18 18:14:14 -0400

    Iteration Drill Complete

--- ruby/iteration_drill.rb
+++ ruby/iteration_drill.rb
@@ -6,25 +6,38 @@ zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars",
 # 1. Iterate through the zombie_apocalypse_supplies array using #each,
 # printing each item in the array separated by an asterisk
 # ----
+zombie_apocalypse_supplies.each do |item|
+  print "#{item}" + " * "
+end
 
 # 2. Create a method to see if a particular item (string) is in the
 # zombie_apocalypse_supplies using #each.
 # For instance: are boots in your list of supplies?
-# ----
+p "---------"
+p zombie_apocalypse_supplies.each.include?("water jug") do |item|
+end
+
 
 # 3. You can't carry too many things, you've only got room in your pack for 5.
 # Remove items in your zombie_apocalypse_supplies in any way you'd like,
 # leaving only 5, using #each.
-# ----
+p "----------"
+# p zombie_apocalypse_supplies.pop(5)
 
 # 4. You found another survivor! This means you can combine your supplies.
 # Create a new combined supplies list out of your zombie_apocalypse_supplies
 # and their supplies below. You should get rid of any duplicate items.
 # Find the built-in method that helps you accomplish this in the Ruby
 # documentation for Arrays.
-other_survivor_supplies = [ "warm clothes", "rations", "compass", "camp stove",
+p "----"
+ other_survivor_supplies = [ "warm clothes", "rations", "compass", "camp stove",
                             "solar battery", "flashlight"]
-# ----
+
+ combined_supplies = zombie_apocalypse_supplies + other_survivor_supplies
+ p combined_supplies.uniq
+
+
+
 
 # Hash Drills
 
@@ -40,16 +53,28 @@ extinct_animals = {
 
 # 1. Iterate through extinct_animals hash using #each, printing each key/value pair
 # with a dash in between the key and value, and an asterisk between each pair.
-# ----
+p "Iterate through Hashes"
+p "----------------------"
+
+extinct_animals.each do |animal, year|
+  print "#{animal} #{year}" + " * "
+end
+
 
 # 2. Keep only animals in extinct_animals if they were extinct before
 # the year 2000, using #each.
-# ----
+p "---------"
+
+#p extinct_animals.keep_if {|animal, year| year < 2000 }
+
 
 # 3. Our calculations were completely off, turns out all of those animals went
 # extinct 3 years before the date provided. Update the values in extinct_animals
 # using #each, so they accurately reflect what year the animal went extinct.
-# ----
+p "___----___"
+
+extinct_animals.each {|animal, year| puts year - 3}
+#how to return the updated key, value pairs?
 
 # 4. You've heard that the following animals might be extinct, but you're not sure.
 # Build a method  using #each that checks if an animal is in the hash and returns true/false.
@@ -59,10 +84,16 @@ extinct_animals = {
 # "Dodo"
 # "Saiga Antelope"
 # Driver code example: is_extinct?(extinct_animals, "Andean Cat")
-# ----
+p "----------------------------"
+
+p extinct_animals.has_key?("Andean Cat")
+p extinct_animals.has_key?("Dodo")
+p extinct_animals.has_key?("Saiga Antelope")
 
 # 5. We just found out that the Passenger Pigeon is actually not extinct!
 # Remove them from extinct_animals and return the key value pair as a two item array.
 # Find a Ruby Hash built-in method that helps you accomplish this or build
 # your own method using #each
 # ----
+
+p extinct_animals.delete_if {|animal, year| animal == "Passenger Pigeon"}
