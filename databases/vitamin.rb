875112018c108f7f8f4afbd53b6389b5034f65cf
jkarlin929 <osida11572@gmail.com>
2017-08-19 18:23:48 -0400

    Created a Regimen Database

--- databases/vitamin.rb
+++ databases/vitamin.rb
@@ -19,5 +19,25 @@ create_table_cmd = <<-SQL
 
 SQL
 
-#create a regimen table
+#create a regimen table for a Senior Home
 db.execute(create_table_cmd)
+
+#add a regimen for all the Seniors
+def create_regimen(db, name, age, vitamin)
+  db.execute("INSERT INTO regimen (name, age, vitamin) VALUES (?, ?, ?)", [name, age, vitamin])
+end
+
+#create a method for a random age
+def age
+  n = rand(55..120)
+end
+
+#make an array of all vitamins
+def vitamin
+  vitamins = ["Vitamin C", "Vitamin K", "Vitamin B12", "CoQ10", "Zinc", "Magnesium", "Fish Oil", "Activated Charcoal", "Iron", "Potassium", "Calcium", "Vitamin A", "Niacin", "Flintstones Gummies"].sample
+end
+
+#generate a regimen for the Seniors
+75.times do
+  create_regimen(db, Faker::Name.name, age, vitamin)
+end
