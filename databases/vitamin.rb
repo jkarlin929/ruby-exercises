f507a47f4b92326819bc3ce8f524dfbe0d96ac68
jkarlin929 <osida11572@gmail.com>
2017-08-19 17:27:35 -0400

    Added Vitamins Table

--- databases/vitamin.rb
+++ databases/vitamin.rb
@@ -6,3 +6,18 @@ require 'faker'
 
 #create sqlite3 database
 db = SQLite3::Database.new("regimen.db")
+
+#make a command
+#this is a string
+create_table_cmd = <<-SQL
+  CREATE TABLE regimen(
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(255),
+    age INT,
+    vitamin VARCHAR(255)
+  )
+
+SQL
+
+#create a regimen table
+db.execute(create_table_cmd)
